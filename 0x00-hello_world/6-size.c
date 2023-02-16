fe23251e2143d969f3fbc27baf79ca54854c8ede
Donald Abua <abuadonald@gmail.com>
2023-02-16 05:31:01 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - prints the size of C types
+ *
+ *Return: void
+ */
+
+int main(void)
+{
+char varChar[];
+int varInt;
+long int varLong;
+long long int varLLong;
+float varFloat;
+
+printf("Size of a char: %zu byte(s)\n",sizeof(varChar));
+printf("Size of a int: %zu byte(s)\n",sizeof(int));
+printf("Size of a long int: %zu byte(s)\n",sizeof(varLong));
+printf("Size of a long long int: %zu byte(s)\n",sizeof(varLLong));
+printf("Size of a float: %zu byte(s)",sizeof(varFloat));
+return (0);
+}
