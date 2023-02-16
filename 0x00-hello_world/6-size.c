fd9d5510688c88beb29f8255514f3e3a6d1cffdf
Donald Abua <abuadonald@gmail.com>
2023-02-16 05:35:10 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ long long int varLLong;
 float varFloat;
 
 printf("Size of a char: %zu byte(s)\n", sizeof(varChar));
-printf("Size of a int: %zu byte(s)\n", sizeof(int));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(varLong));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(varLLong));
 printf("Size of a float: %zu byte(s)\n", sizeof(varFloat));
