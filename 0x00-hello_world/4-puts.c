e9e8d62003af44ebae02f200eec0bb8c25261d82
Donald Abua <abuadonald@gmail.com>
2023-02-16 04:58:44 -0800

    4-put.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Prints strings to display
+ * uses the put function print the string
+ *
+ * Return: void
+ */
 int main(void)
 {
 puts("Programming is like building a multilingual puzzle\n");
