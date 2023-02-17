f4c8e8b2565da6b9f4a4d022271e9a91d9798ffd
Donald Abua <abuadonald@gmail.com>
2023-02-17 12:08:55 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,2 +1,20 @@
 #include <stdio.h>
 
+/**
+ *main - prints all single digit numbers of base 10 \
+starting from 0, followed by a new line
+*Return: int
+*/
+
+int main(void)
+{
+int num = 48;
+
+while (num < 58)
+{
+putchar(48);
+num++;
+}
+putchar('\n');
+return (0);
+}
