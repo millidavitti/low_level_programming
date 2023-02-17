0fde039486ff7d53cde4ef52b1959ba32b963b52
Donald Abua <abuadonald@gmail.com>
2023-02-17 05:18:05 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - prints the alphabet in lowercase, followed by a new line
+ *
+ *Return: void
+ */
+
+int main(void)
+{
+int lower = 97;
+
+while (lower <= 122)
+{
+if (lower == 101 || lower == 113)
+continue;
+putchar(lower);
+lower++;
+}
+putchar('\n');
+return (0);
+}
