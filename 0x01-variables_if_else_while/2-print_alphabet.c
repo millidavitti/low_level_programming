4869d04106c1993306651952a3be7f97ab27481f
Donald Abua <abuadonald@gmail.com>
2023-02-17 04:30:27 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - prints the alphabet in lowercase, followed by a new line.
+ *
+ *Return: void
+ */
+
+int main()
+{
+int ascii = 97;
+
+while(ascii < 123)
+{
+putchar(ascii);
+ascii++;
+}
+return (0);
+}
