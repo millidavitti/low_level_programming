b9b42a13133bbca745cc49a1326c57efd825032f
Donald Abua <abuadonald@gmail.com>
2023-02-21 07:30:11 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_sign - prints the sign of a number.
+ *@n: int
+ *
+ *Return: int
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+return (1);
+else if (n < 0)
+return (-1);
+else
+return (0);
+}
