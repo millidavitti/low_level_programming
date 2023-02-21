4b7aa88d8d02d25983aeee6e36c11ca637ff7494
Donald Abua <abuadonald@gmail.com>
2023-02-21 12:52:14 -0800

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_to_98 - prints all natural numbers from n to 98, \
+followed by a new line
+*@n: int
+*
+*Return: int
+*/
+
+int print_to_98(int n)
+{
+int i;
+for (i = n; i < 99; i++)
+{
+int t, u;
+t = (i - (i % 10)) / 10;
+u = i % 10;
+_putchar(u + '0');
+_putchar(44);
+_putchar(32)
+}
+}
