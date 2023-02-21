290b1cb32a423ffa9c437a5d3598629c0b3b7ab6
Donald Abua <abuadonald@gmail.com>
2023-02-21 13:35:02 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,9 +15,16 @@ int i;
 for (i = n; i < 99; i++)
 {
 int t, u;
+ h = (i - (i % 100)) / 100;
 t = abs((i - (i % 10)) / 10);
 u = abs(i % 10);
-if (t > 0)
+if (abs(h) > 0)
+{
+if (h < 0)
+_putchar(45);
+_putchar(abs(h) + '0');
+}
+else if (t > 0)
 _putchar(t + '0');
 _putchar(u + '0');
 if (i == 98)
