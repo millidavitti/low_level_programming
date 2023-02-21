76e73865b779b3af6a470f5c5d66caa759b0fc42
Donald Abua <abuadonald@gmail.com>
2023-02-21 07:36:39 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,9 +10,21 @@
 int print_sign(int n)
 {
 if (n > 0)
+{
+_putchar(43);
+_putchar(49);
 return (1);
+}
 else if (n < 0)
+{
+_putchar(45);
+_putchar(49);
 return (-1);
+}
 else
+{
+_putchar(48);
+_putchar(48);
 return (0);
+}
 }
