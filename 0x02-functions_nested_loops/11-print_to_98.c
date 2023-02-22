b112d21a3ccd699a707efa4f84cb3390315850c9
Donald Abua <abuadonald@gmail.com>
2023-02-21 16:30:20 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,6 +9,7 @@ followed by a new line
 *Return: void
 */
 
+
 void print_to_98(int n)
 {
 int i;
@@ -17,31 +18,30 @@ for (i = n; i < 99; i++)
 int t, u, h;
 h = (i - (i % 100)) / 100;
 t = ((i - (i % 10)) / 10) % 10;
-u = i % 10;
-
+u = (i % 10);
 if (abs(h) > 0)
 {
 if (h < 0)
-_putchar(45);
-_putchar(abs(h) + '0');
-_putchar(abs(t) + '0');
+putchar(45);
+putchar(abs(h) + '0');
+putchar(abs(t) + '0');
 }
 else if (abs(t) > 0)
 {
 if (t < 0)
-_putchar(45);
-_putchar(abs(t) + '0');
+putchar(45);
+putchar(abs(t) + '0');
 }
 else if(abs(u) > 0){
 if (u < 0)
-_putchar(45);
+putchar(45);
 }
-_putchar(abs(u) + '0'); 
-_putchar(u + '0');
+putchar(abs(u) + '0');
+
 if (i == 98)
 continue;
-_putchar(44);
-_putchar(32);
+putchar(44);
+putchar(32);
 }
-_putchar('\n');
+putchar('\n');
 }
