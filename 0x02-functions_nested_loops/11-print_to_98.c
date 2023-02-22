b02791c0bf50363e733dd8413ad0ac1bf62a1b0a
Donald Abua <abuadonald@gmail.com>
2023-02-21 16:10:02 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,7 +18,6 @@ int t, u, h;
 h = (i - (i % 100)) / 100;
 t = ((i - (i % 10)) / 10) % 10;
 u = abs(i % 10);
-
 if (abs(h) > 0)
 {
 if (h < 0)
@@ -31,7 +30,6 @@ else if (abs(t) > 0)
 if (t < 0)
 _putchar(45);
 _putchar(abs(t) + '0');
-_putchar(t + '0');
 }
 _putchar(u + '0');
 if (i == 98)
