250b939153a153417e33148861637b20408c2331
Donald Abua <abuadonald@gmail.com>
2023-02-23 05:40:59 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,6 +9,8 @@
 void print_diagonal(int n)
 {
 int x, y;
+if (n < 0 || n == 0)
+_putchar('\n');
 for (x = 0; x < n; x++)
 {
 for (y = 0; y < x + 1; y++)
@@ -21,5 +23,4 @@ _putchar(' ');
 }
 _putchar('\n');
 }
-_putchar('\n');
 }
