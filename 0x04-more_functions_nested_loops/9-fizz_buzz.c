71654bebade8dc2b5aec69671e7acd93c359d042
Donald Abua <abuadonald@gmail.com>
2023-02-23 07:35:55 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  *main - multiples of 3 get replaced wtih fizz \
 multiples of 5 get replaced with buzz and \
@@ -7,7 +7,7 @@ multiples of both 3 and 5 get replaced with fizzbuzz
 *return: void
  */
 
-int main(void)
+void fizz_buzz(void)
 {
 int n;
 for (n = 1; n <= 100; n++)
@@ -17,23 +17,41 @@ t = (n - (n % 10)) / 10;
 u = n % 10;
 if (n % 3 == 0 && n % 5 == 0)
 {
-printf("FizzBuzz");
+_putchar(70);
+_putchar(105);
+_putchar(122);
+_putchar(122);
+       
+_putchar(66);
+_putchar(117);
+_putchar(122);
+_putchar(122);
+_putchar(32);
 continue;
 }
 else if (n % 3 == 0)
 {
-printf("Fizz");
+_putchar(70);
+_putchar(105);
+_putchar(122);
+_putchar(122);
+_putchar(32);
+          
 continue;
 }
 else if (n % 5 == 0)
 {
-printf("Buzz");
+_putchar(66);
+_putchar(117);
+_putchar(122);
+_putchar(122);
+_putchar(32);
 continue;
 }
 if (t > 0)
-_putchar(t + '0');
+_putchar(t + '0');      
 _putchar(u + '0');
 _putchar(32);
+      
 }
-return (0);
 }
