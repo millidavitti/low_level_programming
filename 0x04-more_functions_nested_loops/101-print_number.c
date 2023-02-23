75242eb236d796b9958c2bbf0a53193f2b1aca85
Donald Abua <abuadonald@gmail.com>
2023-02-23 14:09:40 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,18 +9,23 @@
 
 void print_number(int n)
 {
-int num, p, i;
+int num, i, p;
 p = 0;
 num = n;
-while (num1 != 0)
+while (num != 0)
 {
-num1 /= 10;
+num /= 10;
 ++p;
-printf("%d\n", p);
 }
 
 for (i = p; i >= 0; i--)
 {
-putchar(((n / (int) pow(10, i - 1)) % 10) + '0');
+int exp, j;
+exp = 1;
+for (j = 0; j < i; j++)
+{
+exp = exp * 10;
+}
+_putchar(((n / exp) % 10) + '0');
 }
 }
