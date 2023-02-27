42ab554569e0eefb20269cf9fd0c9d65ef1e1964
Donald Abua <abuadonald@gmail.com>
2023-02-27 07:58:54 -0800

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ *_strcpy - copies the string pointer value
+ *@dest: char*
+ *@src: char*
+ *Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+char str = strcpy(dest, src);
+return (str);
+}
