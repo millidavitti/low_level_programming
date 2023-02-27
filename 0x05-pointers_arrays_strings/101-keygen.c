bffadd4c30bb6edef096877a83df6743ee3ac0ad
Donald Abua <abuadonald@gmail.com>
2023-02-27 15:40:07 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -21,9 +21,9 @@ for (i = 0; i < 100; i++)
 pass[i] = rand() % 33;
 total += (pass[i] + 48);
 putchar(pass[i] + 48);
-if ((8937 - total) - 48 < 33)
+if ((1981 - total) - 48 < 33)
 {
-n = 8937 - total - 48;
+n = 1981 - total - 48;
 total += n;
 putchar(n + 48);
 break;
