aebdad6f6efc8bd85588eee0c9be711d13f331e8
Donald Abua <abuadonald@gmail.com>
2023-03-02 03:55:21 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
 #pragma once
-
+0;10;1c
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -8,3 +8,4 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *c);
 char *cap_string(char *str);
 char *leet(char *c);
+char *rot13(char *c);
