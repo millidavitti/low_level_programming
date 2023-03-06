e7b9188ea1a072d06699b957b2a3b2cf4f9402a6
Donald Abua <abuadonald@gmail.com>
2023-03-06 10:17:39 -0800

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ *_strpbrk - searches a string for any of a set of bytes.
+ *@s: char
+ *@accept: char set
+ *Return: char*
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+strpbrk(s, accept);
+return (s);
+}
