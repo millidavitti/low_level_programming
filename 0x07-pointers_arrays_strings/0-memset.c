ae83854209f598e7bc9ea64cec69f07bb30d3da7
Donald Abua <abuadonald@gmail.com>
2023-03-06 06:58:58 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,12 +10,6 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
-if (n > strlen(s))
-i = strlen(s) - 2;
-for (i = n; i > 0; i--)
-{
-s[i] = b;
-}
+memset(s, b, n);
 return (s);
 }
