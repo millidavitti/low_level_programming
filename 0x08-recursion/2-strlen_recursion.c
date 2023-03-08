7e21660116bc6130045cd71dec3817310c4423ee
Donald Abua <abuadonald@gmail.com>
2023-03-08 02:44:25 -0800

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+
+/**
+ *_strlen_recursion - returns the length of a string.
+ *@s: string
+ *Return: interger
+ */
+
+int _strlen_recursion(char *s)
+{
+if (!*s)
+return (0);
+return (_strlen_recursion(s + 1) + 1);
+}
