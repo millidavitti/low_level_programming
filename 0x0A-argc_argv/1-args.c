638761c6a7373089ef9017df3a1c0cffcbae3d83
Donald Abua <abuadonald@gmail.com>
2023-03-10 10:00:42 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - prints argument count
+ *@argc: argument count
+ *@argv: array of pointers
+ *Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%s\n", argc);
+return (0);
+}
