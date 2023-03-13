67abd016eb9896678099751fc9aca6b3cea62d52
Donald Abua <abuadonald@gmail.com>
2023-03-13 07:29:55 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,11 +5,12 @@
 initializes it with a specific char.
 *@size: array size
 *@c: char
+*Return: char*
 */
 
 char *create_array(unsigned int size, char c)
 {
-char *arr = malloc(sizeof(char) * size) ? (char *)malloc(sizeof(char) * size) : NULL;
+char *arr = (char *)malloc(sizeof(char) * size);
 unsigned int i;
 if (!size)
 return (NULL);
