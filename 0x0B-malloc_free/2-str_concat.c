d225db12ffcb40a47b612459385b6c48ba35c910
Donald Abua <abuadonald@gmail.com>
2023-03-13 14:24:24 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,23 +8,23 @@
  *@s2: string*
  *Return: string
  */
+
 char *str_concat(char *s1, char *s2)
 {
-int i;
+int i, len;
 char *concat;
-if (s1 == NULL || s2 == NULL)
+if (s1 == NULL && s2 == NULL)
 return (NULL);
-
+else if (s1 == NULL)
+s1 = "";
+else if (s2 == NULL)
+s2 = "";
 concat = (char *)malloc(sizeof(char) * (strlen(s2) + strlen(s1) + 1));
-
 if (!concat)
 return (NULL);
-
-for (i = 0; i < (int)strlen(s1); i++)
+for (i = 0; s1[i]; i++)
 concat[i] = s1[i];
-
-for (i = 0; i < (int)strlen(s2); i++)
+for (i = 0; s2[i]; i++)
 concat[i + strlen(s1)] = s2[i];
-
 return (concat);
 }
