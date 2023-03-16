c9d0141d245394ad3fddb3be0a56ccd5b4f37f66
Donald Abua <abuadonald@gmail.com>
2023-03-16 09:16:09 -0700

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,7 +12,7 @@ int *array_range(int min, int max)
 int *arr, i;
 if (min > max)
 return (NULL);
-arr = malloc(sizeof(int) * (max - min) + 1);
+arr = malloc(sizeof(int) * (max - min + 1));
 if (!arr)
 return (NULL);
 
