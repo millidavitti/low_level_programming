58fa8b3ad0c1dd94fce0b890de160b02bacfd6e7
Donald Abua <abuadonald@gmail.com>
2023-03-17 13:37:53 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,28 +10,33 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-  unsigned int *new_block, i;
-if (new_size > old_size)
-new_block = malloc(old_size + new_size);
-else if (ptr == NULL)
-{
-new_block = malloc(new_size);
-if (!new_block)
-return (NULL);
-return (new_block);
-}
-else if (new_size == old_size)
+char *new_block;
+unsigned int i;
+
+if (new_size == old_size)
 return (ptr);
-else if (!new_size && ptr)
+
+if (new_size == 0 && ptr != NULL)
 {
 free(ptr);
 return (NULL);
 }
 
-if (!new_block)
+if (ptr == NULL)
+{
+new_block = malloc(new_size);
+if (new_block == NULL)
 return (NULL);
+return (new_block);
+}
+
+new_block = malloc(new_size);
+if (new_block == NULL)
+return (NULL);
+
 for (i = 0; i < old_size && i < new_size; i++)
 new_block[i] = ((char *)ptr)[i];
+
 free(ptr);
-return (new_block);
+return (ptr);
 }
