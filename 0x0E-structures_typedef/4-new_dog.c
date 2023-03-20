b4de5a35b623c191ef8451e4bfde101a10c33bdf
Donald Abua <abuadonald@gmail.com>
2023-03-20 12:11:43 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,36 @@
 #include "dog.h"
 #include <stddef.h>
+#include <stdlib.h>
+
+/**
+ *_strlen - string length
+ *@str: string
+ *Return: int
+ */
+
+int _strlen(char *str)
+{
+int i;
+for (i = 0; str[i]; i++)
+;
+return (i);
+}
+
+/**
+ *_strcpy - copy string
+ *@copy: copy
+ *@src: original
+ *Return: char*
+ */
+char *_strcpy(char *copy, char *src)
+{
+int i;
+for (i = 0; src[i]; i++)
+copy[i] = src[i];
+copy[i] = '\0';
+return (copy);
+}
+
 /**
  *new_dog - creates a new dog.
  *@name: name
@@ -10,13 +41,32 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *dog  = struct dog;
+dog_t *dog;
+char *lname, *lowner;
+lname = malloc(sizeof(char *) * (_strlen(name) + 1));
+lowner = malloc(sizeof(char *) * (_strlen(owner) + 1));
+if (!lname)
+{
+free(lname);
+return (NULL);
+}
+else
+lname = _strcpy(lname, name);
+if (!lowner)
+{
+free(lowner);
+return (NULL);
+}
+else
+lowner = _strcpy(lowner, owner);
 
+dog = malloc(sizeof(dog_t *));
 if (!dog)
 return (NULL);
-dog->name = name;
+
+dog->name = lname;
 dog->age = age;
-dog->owner = owner;
+dog->owner = lowner;
 
 return (dog);
 }
