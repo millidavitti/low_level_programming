c61cd8ffe8a7878b9b9f929744fec2521f6c967f
Donald Abua <abuadonald@gmail.com>
2023-03-20 08:33:39 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,12 @@
  *
  *Description: A struct for a dog with name, age, and owner
  */
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
