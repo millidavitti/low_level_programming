073a65bc8df5ebbca0c4364cf817581308102646
Donald Abua <abuadonald@gmail.com>
2023-03-20 07:45:52 -0700

    ifndef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,17 @@
-#pragma
-
+#ifndef DOG_H 
+ #define DOG_H 
+  
+/** 
+  * struct dog - struct dog 
+  * @name: name of dog 
+  * @age: age of dog 
+  * @owner: owner of dog 
+  * 
+  * Description: A struct for a dog with name, age, and owner 
+  */
 struct dog {
   char *name;
   float age;
   char *owner;
 };
+#endif /* DOG_H */
