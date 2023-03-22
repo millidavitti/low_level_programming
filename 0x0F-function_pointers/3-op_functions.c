4c2b55904a7b3f2b7a18e8155d81e7f54a21cc5a
Donald Abua <abuadonald@gmail.com>
2023-03-22 13:12:43 -0700

    calculator

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include <math.h>
+/**
+ *op_add - returns the sum of a and b
+ *@a: number
+ *@b: number
+ *Return: int sum
+ */
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ *op_sub - returns the difference of a and b
+ *@a: number
+ *@b: number
+ *Return: int difference
+ */
+
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ *op_mul - returns the product of a and b
+ *@a: number
+ *@b: number
+ *Return: int product
+ */
+
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ *op_div - returns the division of a by b
+ *@a: number
+ *@b: number
+ *Return: int division
+ */
+
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ *op_mod - returns the remainder of the division of a by b
+ *@a: number
+ *@b: number
+ *Return: int mod
+ */
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
