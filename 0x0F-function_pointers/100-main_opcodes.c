07d3f7339e1712e0f38f50e7247f8c37e3879fe7
Donald Abua <abuadonald@gmail.com>
2023-03-22 13:32:45 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,13 +9,13 @@
 int main(int argc, char *argv[]) {
 if (argc != 2) {
 printf("Error\n");
-return 1;
+exit(1);
 }
 
 int num_bytes = atoi(argv[1]);
 if (num_bytes < 0) {
 printf("Error\n");
-return 2;
+exit(2);
 }
 
 char *start = (char *) main;
