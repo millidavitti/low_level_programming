872e4bffb072e3f158cf1583fc49d468daad01f9
Donald Abua <abuadonald@gmail.com>
2023-03-22 06:00:08 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ *print_name - prints a name.
+ *@name: name to print
+ *@f: callback function
+ *Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
