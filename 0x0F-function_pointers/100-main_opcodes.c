77d508d1c9cca4cb0eccdb9f40d7e645af9ef0bb
Donald Abua <abuadonald@gmail.com>
2023-03-22 13:28:51 -0700

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - entry point
+ *@argc: argument length
+ *@argv: array of argumnents
+ *Return: int
+ */
+int main(int argc, char *argv[]) {
+if (argc != 2) {
+printf("Error\n");
+return 1;
+}
+
+int num_bytes = atoi(argv[1]);
+if (num_bytes < 0) {
+printf("Error\n");
+return 2;
+}
+
+char *start = (char *) main;
+char *end = start + num_bytes;
+
+while (start < end) {
+printf("%02x ", *start & 0xff);
+start++;
+}
+
+printf("\n");
+return 0;
+}
