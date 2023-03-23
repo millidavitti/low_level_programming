d21e1476ed4d7512abea7b4cd63d18ff33ce1e70
Donald Abua <abuadonald@gmail.com>
2023-03-23 06:31:50 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,12 +13,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 va_list args;
 unsigned int i;
 va_start(args, n);
-if (!separator)
-return;
+
 for (i = 0; i < n; i++)
 {
 printf("%d", va_arg(args, int));
-if (i != n - 1)
+if (i != n - 1 && separator != NULL)
 {
 printf("%s", separator);
 }
