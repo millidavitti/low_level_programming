4af4d8d3b02c6b98a0cf70854be342b67714e1ca
Donald Abua <abuadonald@gmail.com>
2023-03-23 07:30:24 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <stdarg.h>
 /**
- *print_all -  prints anything.
+ *loop -  loops.
  *@format: types of arguments passed to the function
+ *@c: char
+ *@s: char *
+ *@f: float
+ *@i: iteration count
+ *@args: va_list
  *Return: void
  */
-void print_all(const char *format, ...)
+void loop(const char *format, char c, char *s, float f, int i, va_list args)
 {
-va_list args;
-int i = 0;
-char c = '\0';
-char *s = NULL;
-float f = 0.0f;
-va_start(args, format);
 while (format && format[i])
 {
 if (format[i] == 'c')
@@ -46,6 +45,22 @@ format[i] == 'f' || format[i] == 's'))
 printf(", ");
 }
 }
+}
+
+/**
+ *print_all -  prints anything.
+ *@format: types of arguments passed to the function
+ *Return: void
+ */
+void print_all(const char *format, ...)
+{
+va_list args;
+int i = 0;
+char c = '\0';
+char *s = NULL;
+float f = 0.0f;
+va_start(args, format);
+loop(format, c, s, f, i, args);
 
 printf("\n");
 va_end(args);
