2eb0013fac53cbad45f91b105d44e75865922f6d
Donald Abua <abuadonald@gmail.com>
2023-04-04 05:04:49 -0700

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -22,4 +22,5 @@ struct listint_s *next;
 size_t print_listint(const listint_t *head);
 size_t listint_len(const listint_t *head);
 listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 #endif	/* LISTS_H */
