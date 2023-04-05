427575c8f6f52642b9ba7a6b44ca3cf1fc903177
Donald Abua <abuadonald@gmail.com>
2023-04-05 13:52:53 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,7 +5,7 @@
 * @head: the head of the list
 * Return: the number of nodes in the list
 */
-size_t get_list_length(const list_t *head)
+size_t list_len(const list_t *head)
 {
 size_t length = 0;
 const list_t *current = head;
