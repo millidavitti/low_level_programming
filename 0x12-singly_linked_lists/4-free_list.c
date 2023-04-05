dbce45a16148f181b72a78611b415b7389e8e0f6
Donald Abua <abuadonald@gmail.com>
2023-04-05 14:36:33 -0700

    free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,17 +2,15 @@
 
 /**
  * free_list - frees a list_t list
- * @head: pointer to the head of the list
- *
+ * @head: head of the list
  * Return: void
  */
 void free_list(list_t *head)
 {
-if (!head)
-return;
-
 list_t *prev = NULL;
 list_t *current = head;
+if (!head)
+return;
 
 while (current)
 {
