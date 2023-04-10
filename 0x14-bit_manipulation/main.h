5671d9c8a2341fdcd6e550e62ea600bc2311f217
Donald Abua <abuadonald@gmail.com>
2023-04-09 18:18:30 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,7 +4,7 @@
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
-
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 #endif /* MAIN_H */
