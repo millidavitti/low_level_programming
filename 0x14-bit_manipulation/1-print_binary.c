cfb9e81da6adc0bc5f871975a014fae011520f04
Donald Abua <abuadonald@gmail.com>
2023-04-09 18:13:13 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,14 +7,17 @@
  *@n: u..d int
  *Return: void
  */
-void print_binary(unsigned long int n) {
+void print_binary(unsigned long int n)
+{
 int i, bit, exp = 0, num;
 num = (n / 2);
-while (num) {
+while (num)
+{
 num = (num / 2);
 exp++;
 }
-for (i = sizeof(n) * exp - (sizeof(n) * exp - exp); i >= 0; i--) {
+for (i = sizeof(n) * exp - (sizeof(n) * exp - exp); i >= 0; i--)
+{
 bit = (n >> i) & 1;
 putchar(bit ? '1' : '0');
 }
