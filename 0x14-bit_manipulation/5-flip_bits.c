b52eb92102780901453b7ac5d31a96ecdc301f4a
Donald Abua <abuadonald@gmail.com>
2023-04-11 04:14:39 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,10 +12,10 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 unsigned long int exc_or = n ^ m;
 unsigned int flips = 0;
 
-while (xor_value)
+while (exc_or)
 {
 flips++;
-exc_or =exc_or & exc_or - 1;
+exc_or =exc_or & (exc_or - 1);
 }
 return (flips);
 }
